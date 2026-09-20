9ae2a2101f0bbaaba123d4c04cd8a1a3ce4aa6c6
caio <caiopaulo2310@gmail.com>
2026-09-20 20:37:32 -0300

    Configuração inicial do projeto Java e Checkstyle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,12 @@
  */
 public class Main {
 
-  /**
-   * Ponto de entrada da aplicação.
-   *
-   * @param args argumentos recebidos pela aplicação
-   */
-  public static void main(String[] args) {
-    System.out.println("PokeSal iniciado!");
-  }
+    /**
+     * Ponto de entrada da aplicação.
+     *
+     * @param args argumentos recebidos pela aplicação
+     */
+    public static void main(String[] args) {
+        System.out.println("PokeSal iniciado!");
+    }
 }

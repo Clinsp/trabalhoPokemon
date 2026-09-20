285315609123053fc1c0e114ea7e93f500d2c955
caio <caiopaulo2310@gmail.com>
2026-09-20 19:41:22 -0300

    Configuração inicial do projeto Java e Checkstyle

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Classe responsável por iniciar o sistema PokeSal.
+ */
+public class Main {
+
+  /**
+   * Ponto de entrada da aplicação.
+   *
+   * @param args argumentos recebidos pela aplicação
+   */
+  public static void main(String[] args) {
+    System.out.println("PokeSal iniciado!");
+  }
+}
